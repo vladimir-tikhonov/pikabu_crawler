ecad16ab4abeabf6582f7eca8b682db0edafba98
v.tikhonov <vladimir@tikhonov.by>
2016-04-02 19:11:06 +0300

    Integrate sidetiq

--- workers/fetch_new_comments_worker.rb
+++ workers/fetch_new_comments_worker.rb
@@ -1,9 +1,15 @@
+require 'sidetiq'
+
 require_relative 'base'
 require_relative 'process_post_worker'
 
 module Workers
   class FetchNewCommentsWorker < Base
-    FETCH_LIMIT = 1000
+    include Sidetiq::Schedulable
+
+    recurrence { hourly.minute_of_hour(0, 15, 30, 45) }
+
+    FETCH_LIMIT = 500
 
     def perform
       first_post_id = elastic_comments.last_indexed_post_id + 1
